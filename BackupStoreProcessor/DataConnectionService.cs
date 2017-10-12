0836b11f15016978eef61a0378a1899f7f40c0a9
Michal Zygmunt <miczyg13@gmail.com>
2017-10-12 09:23:14 +0000

    receiving

---
+++ BackupStoreProcessor/DataConnectionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace BackupStoreProcessor
+{
+    class DataConnectionService
+    {
+        EventHubClient client;
+        public void GetMessagesFromHub(string connectionString)
+        {
+            client = EventHubClient.CreateFromConnectionString(connectionString);
+            var partitions = client.GetRuntimeInformation().PartitionIds;
+            client.GetDefaultConsumerGroup().CreateReceiver("");
+        }
+    }
+}
