4815c05bd92cb7a10d562fad0e2d1f0e6a037f38
Michal Zygmunt <miczyg13@gmail.com>
2017-10-12 11:57:27 +0000

    add table service

---
+++ BackupStoreProcessor/DataTableService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using BackupStoreProcessor.Model;
+
+namespace BackupStoreProcessor
+{
+    class DataTableService
+    {
+        private CloudStorageAccount _account;
+        private CloudTableClient _tableClient;
+        CloudTable _table;
+
+        public void InitConnection(string connectionString, string tableName)
+        {
+            _account = CloudStorageAccount.Parse(connectionString);
+            _tableClient = _account.CreateCloudTableClient();
+            _table = _tableClient.GetTableReference(tableName);
+        }
+
+        public async Task<TableResult> SaveDataToTable(HubMessage data)
+        {
+            TableOperation insertOperation = TableOperation.Insert(data);
+            return await _table.ExecuteAsync(insertOperation);
+           
+        }
+    }
+}
---
+++ BackupStoreProcessor/Model/HubMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace BackupStoreProcessor.Model
+{
+    public class HubMessage : TableEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public string Entity { get; set; }
+    }
+}
