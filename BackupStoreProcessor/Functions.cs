24d4478993e7a4bea2cb27dca8452658f0891b46
Michal Zygmunt <miczyg13@gmail.com>
2017-10-12 11:51:24 +0000

    wip

--- BackupStoreProcessor/Functions.cs
+++ BackupStoreProcessor/Functions.cs
@@ -10,22 +10,28 @@ namespace BackupStoreProcessor
 {
     public class Functions
     {
-
+        private static DataTableService _dataTable;
         // This function will get triggered/executed when a new message is written 
         // on an Azure Queue called queue.
-        static TextWriter _log;
 
         public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
         {
+            _dataTable = new DataTableService();
+            _dataTable.InitConnection(
+                "DefaultEndpointsProtocol=https;AccountName=iotstoragebackup;AccountKey=a3dcqT7uyr8GCcO6/zQlROVlSQmw6/NfB9fGlFBuEaWcdRHKUR0pRVKq/M0yzCuFhDiEB7C/El9IJ3TwecKHDQ==;EndpointSuffix=core.windows.net",
+                "iotbackup");
+
             var service = new DataConnectionService();
             service.OnDataReceived += Service_OnDataReceived;
             service.GetMessagesFromHub("HostName=training-hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=+ru85fPh+aYrEhvu0giINpchKDXBjqcciU2P7/RFnm8=");
-            log.WriteLine(message);
+            //log.WriteLine(message);
         }
 
         private static void Service_OnDataReceived(Microsoft.ServiceBus.Messaging.EventData data)
         {
-            Console.WriteLine("Message received {0}", Encoding.UTF8.GetString(data.GetBytes()));
+            var message = new { CreatedAt = DateTime.Now, Message = Encoding.UTF8.GetString(data.GetBytes()) };
+            var status = _dataTable.SaveDataToTable(message);
+            Console.WriteLine("Message received {0}", status.GetHashCode);
         }
     }
 }
